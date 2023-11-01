1221bc26ad7e2226262420d62d183338a296d275
Thiem Huynh <thiem.huynh0@gmail.com>
2023-11-01 13:57:21 -0400

    using routes.js instead of chained else if

--- my_first_server/helloServer.js
+++ my_first_server/helloServer.js
@@ -1,65 +1,17 @@
-'use strict';
 
-var fs = require('fs');
-var path = require('path');
-var guestsPath = path.join(__dirname, 'guests.json');
+var http   = require('http');
+var routes = require('./routes');
 
-var http = require('http');
-var port = process.env.PORT || 8000;
-
-var server = http.createServer(function(req, res) {
-  if (req.method === 'GET' && req.url === '/guests') {
-    fs.readFile(guestsPath, 'utf8', function(err, guestsJSON) {
-      if (err) {
-        console.error(err.stack);
-        res.statusCode = 500;
-        res.setHeader('Content-Type', 'text/plain');
-        return res.end('Internal Server Error');
-      }
-
-      res.setHeader('Content-Type', 'application/json');
-      res.end(guestsJSON);
-    });
+var handleRequest = function (req, res) {
+  if(routes[req.url] !== undefined) {
+    routes[req.url](req, res);
+  } else {
+    res.end("404, no such route");
   }
-  else if (req.method === 'GET' && req.url === '/guests/0') {
-    fs.readFile(guestsPath, 'utf8', function(err, guestsJSON) {
-      if (err) {
-        console.error(err.stack);
-        res.statusCode = 500;
-        res.setHeader('Content-Type', 'text/plain');
-        return res.end('Internal Server Error');
-      }
+};
 
-      var guests = JSON.parse(guestsJSON);
-      var guestJSON = JSON.stringify(guests[0]);
+var server = http.createServer(handleRequest);
 
-      res.setHeader('Content-Type', 'application/json');
-      res.end(guestJSON);
-    });
-  }
-  else if (req.method === 'GET' && req.url === '/guests/1') {
-    fs.readFile(guestsPath, 'utf8', function(err, guestsJSON) {
-      if (err) {
-        console.error(err.stack);
-        res.statusCode = 500;
-        res.setHeader('Content-Type', 'text/plain');
-        return res.end('Internal Server Error');
-      }
-
-      var guests = JSON.parse(guestsJSON);
-      var guestJSON = JSON.stringify(guests[1]);
-
-      res.setHeader('Content-Type', 'application/json');
-      res.end(guestJSON);
-    });
-  }
-  else {
-    res.statusCode = 404;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Not found');
-  }
-});
-
-server.listen(port, function() {
-  console.log('Listening on port', port);
+server.listen(8000, function() {
+  console.log("Listening...");
 });
---
+++ my_first_server/route.js
@@ -0,0 +1,12 @@
+
+routes = {
+    '/special-message': function(req, res) {
+      res.end("You're SPECIAL");
+    },
+  
+    '/non-special-message': function(req, res) {
+      res.end("You're boring!");
+    }
+  };
+  
+  module.exports = routes;
