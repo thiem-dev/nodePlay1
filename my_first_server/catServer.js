996ed6d3af7e4a5a9b863fe3ccac05e5a54d03f1
Thiem Huynh <thiem.huynh0@gmail.com>
2023-11-01 13:51:29 -0400

    cat server live. hello server has guest path

--- my_first_server/catServer.js
+++ my_first_server/catServer.js
@@ -1,17 +1,17 @@
 // console.log(`Listening on port http://localhost:${port}/`);
 
+
+//catServer.js
 'use strict';
 
 var http = require('http');
 var port = process.env.PORT || 8000;
 
 var server = http.createServer(function(req, res) {
-  if (req.method === 'GET' && req.url === '/guests') {
-    var guests = ['Mary', 'Don'];
-    var guestsJSON = JSON.stringify(guests);
+  if (req.method === 'GET' && req.url === '/cats') {
 
     res.setHeader('Content-Type', 'application/json');
-    res.end(guestsJSON);
+    res.end("meow");
   }
   else {
     res.statusCode = 404;
