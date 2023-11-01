b6b79d839bfb98af4aabeb8a7e989cbc78b946ca
Thiem Huynh <thiem.huynh0@gmail.com>
2023-11-01 13:53:14 -0400

    reading data from a file instead of hardcoded data

--- my_first_server/catServer.js
+++ my_first_server/catServer.js
@@ -1,17 +1,25 @@
-// console.log(`Listening on port http://localhost:${port}/`);
-
-
-//catServer.js
 'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var guestsPath = path.join(__dirname, 'guests.json');
 
 var http = require('http');
 var port = process.env.PORT || 8000;
 
 var server = http.createServer(function(req, res) {
-  if (req.method === 'GET' && req.url === '/cats') {
+  if (req.method === 'GET' && req.url === '/guests') {
+    fs.readFile(guestsPath, 'utf8', function(err, guestsJSON) {
+      if (err) {
+        console.error(err.stack);
+        res.statusCode = 500;
+        res.setHeader('Content-Type', 'text/plain');
+        return res.end('Internal Server Error');
+      }
 
-    res.setHeader('Content-Type', 'application/json');
-    res.end("meow");
+      res.setHeader('Content-Type', 'application/json');
+      res.end(guestsJSON);
+    });
   }
   else {
     res.statusCode = 404;
@@ -21,5 +29,5 @@ var server = http.createServer(function(req, res) {
 });
 
 server.listen(port, function() {
-  console.log(`Listening on port http://localhost:${port}/`);
+  console.log('Listening on port', port);
 });
