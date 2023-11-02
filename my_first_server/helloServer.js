8d81a2afb0d71f499477282b08c924eac3932a2d
Thiem Huynh <thiem.huynh0@gmail.com>
2023-11-02 09:05:41 -0400

    url parser

--- my_first_server/helloServer.js
+++ my_first_server/helloServer.js
@@ -4,6 +4,8 @@
 
 import http from 'http'
 import routes from './routes.js'
+import url from 'url' //for parsedURL = url.parse(req.url, true)
+
 
 var handleRequest = function (req, res) {
   if(routes[req.url] !== undefined) {
@@ -18,3 +20,8 @@ var server = http.createServer(handleRequest);
 server.listen(8000, function() {
   console.log("Listening... on localhost:8000");
 });
+
+
+
+// url.parse()
+// {"protocol":null,"slashes":null,"auth":null,"host":null,"port":null,"hostname":null,"hash":null,"search":null,"query":{},"pathname":"/cats/1","path":"/cats/1","href":"/cats/1"}
